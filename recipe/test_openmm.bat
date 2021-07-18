2cf43ad183b7595d0068ff9a35beef9b5aab1172
peastman <peastman@stanford.edu>
2021-07-17 18:43:04 -0700

    Don't check version number on betas

--- recipe/test_openmm.bat
+++ recipe/test_openmm.bat
@@ -42,9 +42,11 @@ if "%CI%"=="" (
 :: Check version metadata looks ok, only for final releases, RCs are not checked!
 :: See https://stackoverflow.com/a/7006016/3407590 for substring checks in CMD
 if x%PKG_VERSION:rc=%==x%PKG_VERSION% (
-    python -c "from openmm import Platform; v = Platform.getOpenMMVersion(); assert '%PKG_VERSION%' in (v, v+'.0'), v + '!=%PKG_VERSION%'"  || goto :error
-    for /f "usebackq tokens=1" %%a in (`git ls-remote https://github.com/openmm/openmm.git %PKG_VERSION%`) do (
-        python -c "from openmm.version import git_revision; r = git_revision; assert r == '%%a', r + '!=%%a'" || goto :error
+    if x%PKG_VERSION:beta=%==x%PKG_VERSION% (
+        python -c "from openmm import Platform; v = Platform.getOpenMMVersion(); assert '%PKG_VERSION%' in (v, v+'.0'), v + '!=%PKG_VERSION%'"  || goto :error
+        for /f "usebackq tokens=1" %%a in (`git ls-remote https://github.com/openmm/openmm.git %PKG_VERSION%`) do (
+            python -c "from openmm.version import git_revision; r = git_revision; assert r == '%%a', r + '!=%%a'" || goto :error
+        )
     )
 ) else (
     echo "!!! WARNING !!!"
