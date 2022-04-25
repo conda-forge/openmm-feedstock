0b7331e835bea07617b532534d02f886810a5697
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-04-25 12:08:07 -0700

    fix version checks, should port this to main

--- recipe/test_openmm.bat
+++ recipe/test_openmm.bat
@@ -43,10 +43,12 @@ if "%CI%"=="" (
 :: See https://stackoverflow.com/a/7006016/3407590 for substring checks in CMD
 if x%PKG_VERSION:rc=%==x%PKG_VERSION% (
     if x%PKG_VERSION:beta=%==x%PKG_VERSION% (
-        python -c "from openmm import Platform; v = Platform.getOpenMMVersion(); assert '%PKG_VERSION%' in (v, v+'.0'), v + '!=%PKG_VERSION%'"  || goto :error
-        for /f "usebackq tokens=1" %%a in (`git ls-remote https://github.com/openmm/openmm.git %PKG_VERSION%`) do (
+	if x%PKG_VERSION:dev=%==x%PKG_VERSION% (
+            python -c "from openmm import Platform; v = Platform.getOpenMMVersion(); assert '%PKG_VERSION%' in (v, v+'.0'), v + '!=%PKG_VERSION%'"  || goto :error
+            for /f "usebackq tokens=1" %%a in (`git ls-remote https://github.com/openmm/openmm.git %PKG_VERSION%`) do (
             python -c "from openmm.version import git_revision; r = git_revision; assert r == '%%a', r + '!=%%a'" || goto :error
-        )
+         )
+	)
     )
 ) else (
     echo "!!! WARNING !!!"
