9054da5c826527214748f508d5627562aa9f1d9b
peastman <peastman@stanford.edu>
2023-01-26 16:59:48 -0800

    Another attempt at trying to make it happy with the batch file syntax

--- recipe/test_openmm.bat
+++ recipe/test_openmm.bat
@@ -32,21 +32,18 @@ python -c "from openmm import Platform as P; n = P.getNumPlatforms(); assert n =
 
 :: Now let's run a little MD
 cd %LIBRARY_PREFIX%/share/openmm/examples
-:: python benchmark.py --test=rf --seconds=10 --platform=Reference || goto :error
-:: python benchmark.py --test=rf --seconds=10 --platform=CPU || goto :error
-:: if "%CI%"=="" (
-::     python benchmark.py --test=rf --seconds=10 --platform=CUDA  || goto :error
-::     python benchmark.py --test=rf --seconds=10 --platform=OpenCL  || goto :error
-:: )
+python benchmark.py --test=rf --seconds=10 --platform=Reference || goto :error
+python benchmark.py --test=rf --seconds=10 --platform=CPU || goto :error
+if "%CI%"=="" (
+    python benchmark.py --test=rf --seconds=10 --platform=CUDA  || goto :error
+    python benchmark.py --test=rf --seconds=10 --platform=OpenCL  || goto :error
+)
 
 :: Check version metadata looks ok, only for final releases, RCs are not checked!
 :: See https://stackoverflow.com/a/7006016/3407590 for substring checks in CMD
 if x%PKG_VERSION:rc=%==x%PKG_VERSION% (
     if x%PKG_VERSION:beta=%==x%PKG_VERSION% (
         python -c "from openmm import Platform; v = Platform.getOpenMMVersion(); assert '%PKG_VERSION%' in (v, v+'.0'), v + '!=%PKG_VERSION%'"  || goto :error
-        :: for /f "usebackq tokens=1" %%a in (`git ls-remote https://github.com/openmm/openmm.git %PKG_VERSION%`) do (
-        ::     python -c "from openmm.version import git_revision; r = git_revision; assert r == '%%a', r + '!=%%a'" || goto :error
-        :: )
     )
 ) else (
     echo "!!! WARNING !!!"
